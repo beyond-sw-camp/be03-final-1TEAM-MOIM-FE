7424a8e6d0ba99ca0bacd1f1344d287bb4a4881f
MrKeeplearning <fossilhunter@hanmail.net>
2024-04-09 11:22:40 +0900

    [Fix] router/index.js에서 routes에 닫는 대괄호 추가

--- src/router/index.js
+++ src/router/index.js
@@ -5,22 +5,14 @@ import Login from "@/views/LoginView.vue";
 import EventCreate from "@/components/EventCreate.vue";
 
 const routes = [
-    {
-        path: "/",
-        redirect: '/main'
-    },
-    {
-        path: "/main",
-        name: "MainPage",
-        component: MainPage
-    },
-    {
-        path: "/event",
-        name: "EventCreate",
-        component: EventCreate
-    },
-    {path: "/register", name: "register", component: Register},
-    {path: "/", name: "home", component: Login},
+    { path: "/", redirect: '/main' },
+    { path: "/main", name: "MainPage", component: MainPage },
+
+    { path: "/event", name: "EventCreate", component: EventCreate },
+
+    { path: "/register", name: "register", component: Register},
+    { path: "/", name: "home", component: Login }
+];
 
 const router = createRouter({
     history: createWebHistory(),
