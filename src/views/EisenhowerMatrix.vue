8cab852229343dfbe74115ca89bfc5e42e1e1d55
JangEunJi <>
2024-04-17 16:24:42 +0900

    화면에서 drag and drop 기능 추가

--- src/views/EisenhowerMatrix.vue
+++ src/views/EisenhowerMatrix.vue
@@ -1,95 +1,126 @@
 <template>
   <div class="EisenhowerParent">
     <v-container fluid class="pa-0 eisenhower-matrix">
+      <v-col cols="12" class="d-flex flex-column">
+        <!-- Horizontal labels -->
+        <v-row class="emergency-text">
+          <v-col cols="6" class="d-flex justify-center align-end pr-2 label-horizontal"
+            >긴급</v-col
+          >
+          <v-col cols="6" class="d-flex justify-center align-end pl-2 label-horizontal"
+            >긴급하지 않음</v-col
+          >
+        </v-row>
 
-      <v-row class="flex-nowrap">
-        <v-col cols="12" class="d-flex flex-column">
-          <!-- Horizontal labels -->
-          <v-row class="emergency-text">
-            <v-col cols="6" class="d-flex justify-center align-end pr-2 label-horizontal">긴급</v-col>
-            <v-col cols="6" class="d-flex justify-center align-end pl-2 label-horizontal">긴급하지 않음</v-col>
-          </v-row>
+        <!-- 위의 2분면 -->
+        <v-row class="flex-grow-1">
+          <v-col cols="2" class="d-flex align-end flex-column label-vertical-container">
+            <div class="label-vertical">중요</div>
+          </v-col>
 
-          <!-- 위의 2분면 -->
-          <v-row class="flex-grow-1">
-            <v-col cols="2" class="d-flex align-end flex-column label-vertical-container">
-              <div class="label-vertical">중요</div>
-            </v-col>
-
-            <!-- 1분면 -->
-            <v-col cols="5" class="border-right border-bottom pa-3">
-              <v-card id="Q1" class="fill-height" dark>
-                <v-card-text class="subtitle-1">
-                  <div v-for="event in eventsQ1" :key="event.id" class="event-item">
-                    {{ event.title }} - {{ formatDate(event.startDate) }}
+          <!-- 1분면 -->
+          <v-col cols="5" class="border-right border-bottom pa-3">
+            <v-card id="Q1" class="fill-height" dark>
+              <draggable
+                class="drag-area"
+                :list="eventsQ1"
+                group="events"
+                @change="updateEvents"
+              >
+                <template #item="{ element }">
+                  <div :key="element.id" class="event-item">
+                    {{ element.title }} - {{ formatDate(element.startDate) }}
                   </div>
-                </v-card-text>
-              </v-card>
-            </v-col>
+                </template>
+              </draggable>
+            </v-card>
+          </v-col>
 
-            <!-- 2분명 -->
-            <v-col cols="5" class="border-bottom pa-3">
-              <v-card id="Q2" class="fill-height" dark>
-                <v-card-text class="subtitle-1">
-                  <div v-for="event in eventsQ2" :key="event.id" class="event-item">
-                    {{ event.title }} - {{ formatDate(event.startDate) }}
+          <!-- 2분명 -->
+          <v-col cols="5" class="border-bottom pa-3">
+            <v-card id="Q2" class="fill-height" dark>
+              <draggable
+                class="drag-area"
+                :list="eventsQ2"
+                group="events"
+                @change="updateEvents"
+              >
+                <template #item="{ element }">
+                  <div :key="element.id" class="event-item">
+                    {{ element.title }} - {{ formatDate(element.startDate) }}
                   </div>
-                </v-card-text>
-              </v-card>
-            </v-col>
+                </template>
+              </draggable>
+            </v-card>
+          </v-col>
 
+          <v-col cols="2"></v-col>
+        </v-row>
 
-            <v-col cols="2" class="d-flex align-end flex-column label-vertical-container">
-              <div class="label-vertical">중요하지 않음</div>
-            </v-col>
-          </v-row>
+        <!-- 밑의 2분면 -->
+        <v-row class="flex-grow-1">
+          <v-col cols="2" class="d-flex align-end flex-column label-vertical-container">
+            <div class="label-vertical">중요하지 않음</div>
+          </v-col>
 
-          <!-- 밑의 2분면 -->
-          <v-row class="flex-grow-1">
-            <v-col cols="2"></v-col>
-            <v-col cols="5" class="border-right pa-3">
-
-              <!-- 3분면 -->
-              <v-card id="Q3" class="fill-height" dark>
-                <v-card-text class="subtitle-1">
-                  <div v-for="event in eventsQ3" :key="event.id" class="event-item">
-                    {{ event.title }} - {{ formatDate(event.startDate) }}
+          <!-- 3분면 -->
+          <v-col cols="5" class="border-right pa-3">
+            <v-card id="Q3" class="fill-height" dark>
+              <draggable
+                class="drag-area"
+                :list="eventsQ3"
+                group="events"
+                @change="updateEvents"
+              >
+                <template #item="{ element }">
+                  <div :key="element.id" class="event-item">
+                    {{ element.title }} - {{ formatDate(element.startDate) }}
                   </div>
-                </v-card-text>
-              </v-card>
-            </v-col>
+                </template>
+              </draggable>
+            </v-card>
+          </v-col>
 
-
-            <!-- 4분면 -->
-            <v-col cols="5" class="pa-3">
-              <v-card id="Q4" class="fill-height" dark>
-                <v-card-text class="subtitle-1">
-                  <div v-for="event in eventsQ4" :key="event.id" class="event-item">
-                    {{ event.title }} - {{ formatDate(event.startDate) }}
+          <!-- 4분면 -->
+          <v-col cols="5" class="pa-3">
+            <v-card id="Q4" class="fill-height" dark>
+              <draggable
+                class="drag-area"
+                :list="eventsQ4"
+                group="events"
+                @change="updateEvents"
+              >
+                <template #item="{ element }">
+                  <div :key="element.id" class="event-item">
+                    {{ element.title }} - {{ formatDate(element.startDate) }}
                   </div>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
+                </template>
+              </draggable>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
     </v-container>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-import moment from 'moment'; // 날짜 포맷을 위해 moment.js 사용
+import axios from "axios";
+import moment from "moment"; // 날짜 포맷을 위해 moment.js 사용
+import draggable from "vuedraggable";
+
 export default {
+  components: {
+    draggable,
+  },
   data() {
     return {
       eventsQ1: [], // Q1 분면의 이벤트 데이터를 저장할 배열
       eventsQ2: [], // Q2 분면의 이벤트 데이터
       eventsQ3: [], // Q3 분면의 이벤트 데이터
       eventsQ4: [], // Q4 분면의 이벤트 데이터
-      authToken: '',
-
-    }
+      authToken: "",
+    };
   },
 
   created() {
@@ -97,20 +128,23 @@ export default {
     // console.log("Current auth token:", this.authToken); // 현재 토큰 값 로그 출력
     if (this.authToken) {
       this.connectSSE(); // SSE 연결 설정
-      this.fetchEvents('Q1'); // 이벤트 데이터 가져오기
-      this.fetchEvents('Q2');
-      this.fetchEvents('Q3');
-      this.fetchEvents('Q4');
+      this.fetchEvents("Q1"); // 이벤트 데이터 가져오기
+      this.fetchEvents("Q2");
+      this.fetchEvents("Q3");
+      this.fetchEvents("Q4");
     }
   },
 
   methods: {
+    updateEvents(event) {
+      console.log("Draggable event:", event);
+    },
     getAuthToken() {
-      const token = localStorage.getItem('accessToken');
+      const token = localStorage.getItem("accessToken");
       if (!token) {
         alert("로그인 후 이용해 주세요");
-        this.$router.push('/login'); // Vue Router를 사용해 로그인 페이지로 리다이렉트
-        return '';
+        this.$router.push("/login"); // Vue Router를 사용해 로그인 페이지로 리다이렉트
+        return "";
       }
       return token;
     },
@@ -121,12 +155,14 @@ export default {
         return;
       }
       const headers = { Authorization: `Bearer ${this.authToken}` }; // 헤더 설정 방식을 통일
-      const eventSource = new EventSource(`${process.env.VUE_APP_API_BASE_URL}/connect`, { headers });
+      const eventSource = new EventSource(`${process.env.VUE_APP_API_BASE_URL}/connect`, {
+        headers,
+      });
       eventSource.onmessage = function (event) {
-        console.log('New event from server:', event.data);
+        console.log("New event from server:", event.data);
       };
       eventSource.onerror = function (error) {
-        console.error('EventSource failed:', error);
+        console.error("EventSource failed:", error);
         eventSource.close();
       };
     },
@@ -144,23 +180,24 @@ export default {
       try {
         const response = await axios.get(url, { headers });
         if (response.data.success && response.data.data) {
-          this[`events${matrix}`] = response.data.data;  // 직접 data 필드를 참조
+          this[`events${matrix}`] = response.data.data; // 직접 data 필드를 참조
           console.log(`Data received for ${matrix}:`, this[`events${matrix}`]);
         } else {
           console.error(`No data received for ${matrix}`, response.data);
         }
       } catch (error) {
-        console.error(`Error fetching events for ${matrix}:`, error.response ? error.response.data : "No response");
+        console.error(
+          `Error fetching events for ${matrix}:`,
+          error.response ? error.response.data : "No response"
+        );
       }
-
     },
     formatDate(value) {
-      return value ? moment(String(value)).format('MM/DD/YYYY hh:mm') : '';
+      return value ? moment(String(value)).format("MM/DD/YYYY hh:mm") : "";
     },
   },
-}
+};
 </script>
-
 
 <style scoped>
 .EisenhowerParent {
@@ -171,7 +208,6 @@ export default {
   /* 오른쪽 끝에 위치 사이드바 위치에 따라 바꾸기!!! */
   top: 10vh;
   /* 헤더의 높이값에따라 바꾸기!!! */
-
 }
 
 .eisenhower-matrix {
@@ -189,7 +225,6 @@ export default {
 
 .label-vertical {
   writing-mode: tb-rl;
-
 }
 
 .label-vertical-container {
@@ -200,7 +235,6 @@ export default {
 .label-horizontal {
   font-size: 1.25rem;
   /* Font size for horizontal labels */
-
 }
 
 .fill-height {
@@ -217,18 +251,18 @@ export default {
 }
 
 #Q1 {
-  background: #F3676B;
+  background: #f3676b;
 }
 
 #Q2 {
-  background: #F08D75
+  background: #f08d75;
 }
 
 #Q3 {
-  background: #8FDCB2
+  background: #8fdcb2;
 }
 
 #Q4 {
-  background: #4672D3
+  background: #4672d3;
 }
 </style>
